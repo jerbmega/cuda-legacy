d52e9a1ab6efb89494e17ededa2c344593c46562
Connor Baker <ConnorBaker01@gmail.com>
2025-10-27 10:49:53 -0700

    ci: add more hydra jobs
    Signed-off-by: Connor Baker <ConnorBaker01@gmail.com>

--- ci/flakeModule.nix
+++ ci/flakeModule.nix
@@ -26,7 +26,43 @@
         overlays = [ inputs.self.overlays.default ];
       };
 
-      hydraJobs = import ./hydraJobs.nix { inherit lib pkgs; };
+      hydraJobs =
+        let
+          # NOTE: Not all packages are written to support earlier CUDA versions.
+          # For example, cupy needs the CUDA profiler API, which isn't available in early versions.
+          # NOTE: Our overlays do not change the major-versioned or unversioned package sets (e.g., cudaPackages_12,
+          # cudaPackages).
+          # NOTE: We do not build for non-default capabilities (e.g., Jetson devices, non-baseline feature sets, etc.).
+          cudaPackageSetNames =
+            # Only supported by x86_64-linux
+            lib.optionals (system == "x86_64-linux") [
+              "cudaPackages_11_4"
+              "cudaPackages_11_5"
+              "cudaPackages_11_6"
+              "cudaPackages_11_7"
+            ]
+            ++ [
+              "cudaPackages_11_8"
+              "cudaPackages_12_0"
+              "cudaPackages_12_1"
+              "cudaPackages_12_2"
+              "cudaPackages_12_3" # Not supported by Jetson
+              "cudaPackages_12_4"
+              "cudaPackages_12_5"
+              "cudaPackages_12_6"
+              # There is no 12.7 release
+              "cudaPackages_12_8"
+              "cudaPackages_12_9"
+              "cudaPackages_13_0" # Not supported by Jetson prior to Thor
+            ];
+        in
+        lib.genAttrs cudaPackageSetNames (
+          cudaPackageSetName:
+          import ./mkHydraJobs.nix {
+            inherit lib;
+            inherit (pkgs.${cudaPackageSetName}) pkgs;
+          }
+        );
 
       legacyPackages = pkgs;
     };
---
+++ ci/mkHydraJobs.nix
@@ -0,0 +1,120 @@
+# Produces a Hydra job set for an instance of Nixpkgs using the default CUDA package set version.
+{ lib, pkgs }:
+let
+  recursiveMapPackages =
+    let
+      canEvalDrvPath = drv: (builtins.tryEval (builtins.seq drv.drvPath drv.drvPath)).success;
+    in
+    lib.concatMapAttrs (
+      name: value:
+      if lib.isDerivation value then
+        # If we can evaluate the drvPath, trim it for hydraJobs, otherwise include it as-is so we get
+        # an error when we try to evaluate/build the package.
+        { ${name} = if canEvalDrvPath value then lib.hydraJob value else value; }
+      else if value.recurseForDerivations or false || value.recurseForRelease or false then
+        { ${name} = recursiveMapPackages value; }
+      else
+        { }
+    );
+
+  # NOTE: Not all packages are written to support earlier CUDA versions.
+  # For example, cupy needs the CUDA profiler API, which isn't available in early versions.
+  # NOTE: Our overlays do not change the major-versioned or unversioned package sets (e.g., cudaPackages_12,
+  # cudaPackages).
+  jobs = recursiveMapPackages {
+    inherit (pkgs)
+      blas
+      blender
+      cctag # Failed in https://github.com/NixOS/nixpkgs/pull/233581
+      cholmod-extra
+      colmap
+      ctranslate2
+      faiss
+      ffmpeg-full
+      gimp
+      gpu-screen-recorder
+      lapack
+      lightgbm
+      llama-cpp
+      magma
+      meshlab
+      monado # Failed in https://github.com/NixOS/nixpkgs/pull/233581
+      mpich
+      noisetorch
+      ollama
+      onnxruntime
+      opencv
+      openmpi
+      openmvg
+      openmvs
+      opentrack
+      openvino
+      pixinsight # Failed in https://github.com/NixOS/nixpkgs/pull/233581
+      qgis
+      rtabmap
+      saga
+      suitesparse
+      sunshine
+      truecrack-cuda
+      tts
+      ucx
+      ueberzugpp # Failed in https://github.com/NixOS/nixpkgs/pull/233581
+      wyoming-faster-whisper
+      xgboost
+      ;
+
+    cudaPackages = lib.recurseIntoAttrs pkgs.cudaPackages;
+
+    gst_all_1 = lib.recurseIntoAttrs {
+      inherit (pkgs.gst_all_1) gst-plugins-bad;
+    };
+
+    obs-studio-plugins = lib.recurseIntoAttrs {
+      inherit (pkgs.obs-studio-plugins) obs-backgroundremoval;
+    };
+
+    python3Packages = lib.recurseIntoAttrs {
+      inherit (pkgs.python3Packages)
+        catboost
+        cupy
+        faiss
+        faster-whisper
+        flax
+        gpt-2-simple
+        grad-cam
+        jax
+        jaxlib
+        keras
+        kornia
+        mmcv
+        mxnet
+        numpy # Only affected by MKL?
+        onnx
+        openai-whisper
+        opencv4
+        opensfm
+        pycuda
+        pymc
+        pyrealsense2WithCuda
+        pytorch-lightning
+        scikit-image
+        scikit-learn # Only affected by MKL?
+        scipy # Only affected by MKL?
+        spacy-transformers
+        tensorflow
+        tensorflow-probability
+        tesserocr
+        tiny-cuda-nn
+        torch
+        torchaudio
+        torchvision
+        transformers
+        triton
+        ttstokenizer
+        vidstab
+        vllm
+        ;
+    };
+  };
+in
+jobs
